6d601d282361cfe20089bd77eebf4af7f7422c4e
snehjeetc12 <csnehjeet12@gmail.com>
2021-04-22 22:08:37 +0530

    Added method to pass name as query and as path variable

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.component.DemoBean;
+import com.example.demo.controller.HelloRestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloWorldApplication {
-
+	
+	public static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		logger.debug("Welcome to spring concept demo");
+		ConfigurableApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args);
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		logger.debug("Demo Bean = " + demoBean.toString());
+	
 	}
 
 }
--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +14,16 @@ public class HelloRestController {
 	//curl localhost:8080/hello -w "\n"
 	@RequestMapping(value = {"", "/", "/home"})
 	public String sayHello(){
-		return "Hello Worldoooo!";
+		return "Hello From Bridgelabz!";
+	}
+	
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name;
+	}
+	
+	@GetMapping("/param/{name}")
+	public String helloTo(@PathVariable String name) {
+		return "Hello " + name + " From Bridgelabz"; 
 	}
 }

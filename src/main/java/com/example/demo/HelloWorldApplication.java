a34848fdfa847abeab068e0de7b0d866d4b7ccf0
snehjeetc12 <csnehjeet12@gmail.com>
2021-04-22 23:14:49 +0530

    Refactored

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -1,12 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.controller.HelloRestController;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootApplication
 public class HelloWorldApplication {
